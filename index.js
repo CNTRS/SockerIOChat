0449dfd45249b127c41f6ae55f6e7e9dbf90dcb6
CNTRS <m.conteras.calzon@gmail.com>
2015-07-14 19:08:56 +0200

    logEvent function added

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
 server.listen(port, function () {
-  console.log('Server listening at port %d', port);
+  // console.log('Server listening at port %d', port);
+  logEvent('listen', null, port);
 });
 
 // Routing
@@ -28,6 +29,7 @@ io.on('connection', function (socket) {
       username: socket.username,
       message: data
     });
+    logEvent('new message', socket.username, data);
   });
 
   // when the client emits 'add user', this listens and executes
@@ -46,6 +48,7 @@ io.on('connection', function (socket) {
       username: socket.username,
       numUsers: numUsers
     });
+    logEvent('add user', socket.username, numUsers);
   });
 
   // when the client emits 'typing', we broadcast it to others
@@ -74,6 +77,46 @@ io.on('connection', function (socket) {
         username: socket.username,
         numUsers: numUsers
       });
+      logEvent('disconnect', socket.username, numUsers);
     }
   });
 });
+var logEvent = function(type, param1, param2){
+	var mes = (new Date().getMonth()+1)
+	if(mes<10){mes = "0" + mes.toString();}
+	var day = new Date().getDate()
+	if(day<10){day = "0" + day.toString();}
+	var hour = new Date().getHours()
+	if(hour<10){hout = "0" + hour.toString();}
+	var min =new Date().getMinutes()
+	if(min<10){min = "0" + min.toString();}
+	var sec = new Date().getSeconds()
+	if(sec<10){sec = "0" + sec.toString();}
+	var date = ""+ new Date().getFullYear().toString() + "/" + mes + "/" + day + ", " + hour + ":" + min + ":" + sec;
+	var logMess = "* " + date + " - ";
+	switch(type){
+    case 'add user':
+      logMess += " USER \"" + param1 + "\" CONNECTED, there's ";
+      if(param2>1 || param2==0){logMess +="" + param2 + " participants.";}
+      else{logMess += "" + param2 + " participant.";}
+      console.log(logMess);
+      break;
+    case 'new message':
+      logMess += " MESSAGE FROM \"" + param1 +": " + param2 + ".";
+      break;
+    case 'disconnect':
+      logMess += " USER \"" + param1 + "\" DISCONNECTED, there's ";
+      if(param2>1){logMess +="" + param2 + " participants.";}
+      else{logMess += "" + param2 + " participant.";}
+      console.log(logMess);
+      break;
+    default:
+      logMess += "-- SERVER listening at PORT " + param2 +".";
+      console.log(logMess);
+      break;
+  }
+  //console.log(logMess);
+  fs.appendFile('./activity.log', logMess + '\r\n', function (err){
+    if(err){throw err;}
+  });
+}
